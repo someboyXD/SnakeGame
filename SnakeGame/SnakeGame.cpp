c2718814085f9ea8a5742f6cf7a5aacdd87bb502
SomeboyXD <letryyt@gmail.com>
2025-12-18 02:51:05 +0300

    Version 1.0.3

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -1,17 +1,16 @@
-﻿// SnakeGame.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-
-#include <vector>
+﻿#include <vector>
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
 #include <string>
 
-#define snake_symbol '#'
+#define snake_body_symbol '#'
 #define snake_head_symbol '%'
+#define snake_dead_symbol 'X'
 #define fruit_symbol '0'
 
 #define map_scale_Y 20
-#define map_scale_X 41
+#define map_scale_X 40
 
 #define arrow_LEFT 37
 #define arrow_RIGHT 39
@@ -22,6 +21,7 @@ using namespace std;
 
 bool game = true;
 
+// singly linked list
 class Snake {
 private:
     vector<int> curr_pos = { 0, 0 };
@@ -36,6 +36,8 @@ public:
         curr_pos[1] = y;
 
         next_Snake = nullptr;
+
+        // cosmetic option
         isHead = _isHead;
     }
 
@@ -50,6 +52,18 @@ public:
 
         if (temp->next_Snake == nullptr) {
             temp->next_Snake = new Snake(temp->prev_pos[0], temp->prev_pos[1], false);
+        }
+    }
+
+    void add(int x, int y) {
+        Snake* temp = this;
+
+        while (temp->next_Snake != nullptr) {
+            temp = temp->next_Snake;
+        }
+
+        if (temp->next_Snake == nullptr) {
+            temp->next_Snake = new Snake(x, y, false);
         }
     }
 
@@ -70,6 +84,8 @@ public:
 };
 
 void DrawMap(vector<vector<char>> map, int totalFruits) {
+    // it's easier for the console to output everything with a single string than with several chars, as I understand it...
+    // (i wanted to use the console buffer)
     string str = "";
     str.reserve(map_scale_X * (map_scale_Y + 1));
 
@@ -84,20 +100,27 @@ void DrawMap(vector<vector<char>> map, int totalFruits) {
     cout << str << flush;
 }
 
+void game_over(vector<vector<char>>& map) {
+    game = false;
+    for (int y = 0; y < map_scale_Y; y++) {
+        for (int x = 0; x < map_scale_X; x++) {
+            if (map[y][x] == snake_head_symbol) {
+                map[y][x] = snake_dead_symbol;
+            }
+        }
+    }
+}
+
 void SetupMap(vector<vector<char>>& map) {
     for (int i = 0; i < map_scale_Y; i++) {
         for (int j = 0; j < map_scale_X; j++) {
-            if (j == map_scale_X - 1) {
-                map[i][j] = '|';
-                continue;
-            }
             map[i][j] = ' ';
         }
     }
 }
 
 void SpawnFruit(vector<vector<char>>& map) {
-    // Check fruit already exist and if is it then leave from function
+    // check fruit already exist and if is it then leave from function
     for (int i = 0; i < map_scale_Y; i++) {
         for (int j = 0; j < map_scale_X; j++) {
             if (map[i][j] == fruit_symbol)
@@ -105,14 +128,17 @@ void SpawnFruit(vector<vector<char>>& map) {
         }
     }
 
+    // take random values
     int randomPlaceX = rand() % (map_scale_X - 2);
     int randomPlaceY = rand() % (map_scale_Y - 1);
 
-    while (map[randomPlaceY][randomPlaceX] == snake_symbol) {
+    // if there is something on these values, then change them until it is empty
+    while (map[randomPlaceY][randomPlaceX] == snake_body_symbol or map[randomPlaceY][randomPlaceX] == snake_head_symbol) {
         randomPlaceX = rand() % (map_scale_X - 2);
         randomPlaceY = rand() % (map_scale_Y - 1);
     }
 
+    // spawn fruit
     map[randomPlaceY][randomPlaceX] = fruit_symbol;
 }
 
@@ -121,8 +147,10 @@ Snake* SpawnSnake(vector<vector<char>>& map) {
     int randomPlaceY = rand() % (map_scale_Y - 1);
 
     Snake* snake = new Snake(randomPlaceX, randomPlaceY, true);
+    snake->add(randomPlaceX, randomPlaceY - 1);
 
-    map[snake->get_curr_position()[1]][snake->get_curr_position()[0]] = snake_symbol;
+    map[randomPlaceY][randomPlaceX] = snake_head_symbol;
+    map[randomPlaceY - 1][randomPlaceX] = snake_body_symbol;
 
     return snake;
 }
@@ -142,19 +170,25 @@ void MoveSnake(vector<vector<char>>& map, int KeyPressedCode, int* p_totalFruits
     default: break;
     }
 
+    // no borders logistic
     if (curr_pos[0] == map_scale_X - 1) { curr_pos[0] = 0; }
-    if (curr_pos[0] < 0) { curr_pos[0] = map_scale_X - 2; }
+    if (curr_pos[0] < 0) { curr_pos[0] = map_scale_X - 1; }
     if (curr_pos[1] == map_scale_Y) { curr_pos[1] = 0; }
-    if (curr_pos[1] < 0) { curr_pos[1] = map_scale_Y; }
+    if (curr_pos[1] < 0) { curr_pos[1] = map_scale_Y - 1; }
 
-    if (map[curr_pos[1]][curr_pos[0]] == snake_symbol) { game = false; return; }
+    // if the snake's body is in front of it, then we launch a "game over"
+    if (map[curr_pos[1]][curr_pos[0]] == snake_body_symbol) { game_over(map); return; }
+
+    // if there is a fruit in front of the snake, then create a new body and remove the fruit
     if (map[curr_pos[1]][curr_pos[0]] == fruit_symbol) { p_snake->add(); *p_totalFruits = *p_totalFruits + 1; map[curr_pos[1]][curr_pos[0]] = ' '; }
+
+    // moving the snake
     p_snake->set_new_position(curr_pos[0], curr_pos[1]);
 
-    // clear old snake on map
+    // clear snake from map
     for (int y = 0; y < map_scale_Y; y++) {
         for (int x = 0; x < map_scale_X; x++) {
-            if (map[y][x] == snake_symbol or map[y][x] == snake_head_symbol) {
+            if (map[y][x] == snake_body_symbol or map[y][x] == snake_head_symbol) {
                 map[y][x] = ' ';
             }
         }
@@ -166,12 +200,7 @@ void MoveSnake(vector<vector<char>>& map, int KeyPressedCode, int* p_totalFruits
         for (int y = 0; y < map_scale_Y; y++) {
             for (int x = 0; x < map_scale_X; x++) {
                 if (x == temp->get_curr_position()[0] && y == temp->get_curr_position()[1]) {
-                    if (temp->isHead) {
-                        map[y][x] = snake_head_symbol;
-                    }
-                    else {
-                        map[y][x] = snake_symbol;
-                    }
+                    map[y][x] = temp->isHead ? snake_head_symbol : snake_body_symbol;
                 }
             }
         }
@@ -182,7 +211,7 @@ void MoveSnake(vector<vector<char>>& map, int KeyPressedCode, int* p_totalFruits
 
 int main()
 {
-    system("mode con cols=41 lines=22");
+    system("mode con cols=41 lines=23");
     srand(time(0));
 
     vector<vector<char>> map(map_scale_Y, vector<char>(map_scale_X));
@@ -197,13 +226,22 @@ int main()
     SpawnFruit(map);
     DrawMap(map, totalFruits);
 
+    cout << "press any button to start\n";
     _getch();
+    system("mode con cols=41 lines=22");
 
     while (game) {
 
-        for (auto keyCode : keysCode)
-            if (GetAsyncKeyState(keyCode) != 0)
+        for (auto keyCode : keysCode) {
+            if (GetAsyncKeyState(keyCode) != 0) {
+                if (keyCode == arrow_DOWN && KeyPressedCode == arrow_UP) { continue; }
+                if (keyCode == arrow_UP && KeyPressedCode == arrow_DOWN) { continue; }
+                if (keyCode == arrow_RIGHT && KeyPressedCode == arrow_LEFT) { continue; }
+                if (keyCode == arrow_LEFT && KeyPressedCode == arrow_RIGHT) { continue; }
+
                 KeyPressedCode = keyCode;
+            }
+        }
 
         SpawnFruit(map);
         MoveSnake(map, KeyPressedCode, &totalFruits, snake);
